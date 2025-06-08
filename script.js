8c4d94d6c3c9f37236c0c027a0609dd51c3d799d
Jeshuayam <jeshuayam3024@gmail.com>
2025-06-07 22:11:37 -0600

    Estilos en el index, menú de hamburgesa masomenos

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Función para asignar el aria-expanded como false cuando esta retraido
+ */
+function desplegarMenu() {
+      const navList = document.getElementById("nav-list");
+      const button = document.querySelector(".menu-toggle");
+      const expanded = button.getAttribute("aria-expanded") === "true";
+      button.setAttribute("aria-expanded", !expanded);
+      navList.classList.toggle("show");
+    }
